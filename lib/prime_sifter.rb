13b615051d35e4aa605d51bf4134d4cc03056099
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-09 11:05:20 -0800

    Pass first four tests for prime_sifter, refactor
    Co-authored-By: Chase Stokes <chasehstokes@gmail.com>
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- lib/prime_sifter.rb
+++ lib/prime_sifter.rb
@@ -5,16 +5,12 @@ class Prime_sifter
     @number=number
   end
   def find_primes
-    if @number==2
-      return 2
-    else 
       arr=*(2..@number)
       arr.each do |element|
         arr.reject! do |a| 
           if a!=element  
             a%element==0 
           end
-        end
       end
     end
     arr
--- spec/prime_sifter_spec.rb
+++ spec/prime_sifter_spec.rb
@@ -4,10 +4,18 @@ require('prime_sifter.rb')
 describe('#find_primes') do
   newObj = Prime_sifter.new(8)
   newObj2 = Prime_sifter.new(50)
+  newObj3 = Prime_sifter.new(1)
+  newObj4 = Prime_sifter.new(2)
   it("should return array of 2,3,5,7 for number 8") do
     expect(newObj.find_primes).to(eq([2,3,5,7]))
   end
   it("should return array of 2,3,5,7..47 for number 50") do
     expect(newObj2.find_primes).to(eq([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]))
+  end
+  it("should return array of [] for number 1") do
+    expect(newObj3.find_primes).to(eq([]))
+  end
+  it("should return array of 2 for number 2") do
+    expect(newObj4.find_primes).to(eq([2]))
   end
 end
